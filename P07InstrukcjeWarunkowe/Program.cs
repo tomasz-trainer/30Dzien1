8d974539f0d570893a0d3a011117b6cd3d9c66d6
tomasz <csharp@406-00>
2026-06-18 13:57:09 +0200

    ok

---
+++ P07InstrukcjeWarunkowe/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07InstrukcjeWarunkowe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swój wzrost");
+            int wzrost = Convert.ToInt32(Console.ReadLine());
+
+            // wypisz "wysoki" gdy wzrost wyższy niz 180 wpp. wypisz niski 
+
+
+            bool c1 = wzrost > 180;
+
+            if (c1)
+            {
+                Console.WriteLine("wysoki");
+            }
+            else
+            {
+                Console.WriteLine("niski");
+            }
+
+
+            if(c1)
+                Console.WriteLine($"twój wzrost to {wzrost} i jesteś wysoki");
+            else if (wzrost > 160)
+                Console.WriteLine($"twój wzrost to {wzrost} i jesteś średni");
+            else if (wzrost > 150)
+                Console.WriteLine($"twój wzrost to {wzrost} i jesteś niski");
+            else
+                Console.WriteLine($"twój wzrost to {wzrost} i jesteś bardzo niski");
+
+
+            // zapobieganie powtarzajacym sie kawalom kodu 
+            string typ;
+            if (wzrost > 180)
+                typ = "wysoki";
+            else if (wzrost > 160)
+                typ = "średni";
+            else if (wzrost > 150)
+                typ = "niski";
+            else
+                typ = "bardzo niski";
+
+            string raport = $"twój wzrost to {wzrost} i jesteś {typ}";
+
+
+            // operator trój-argumentowy 
+            string wyn;
+            if (wzrost > 180)
+                wyn = "wysoki";
+            else
+                wyn = "niski";
+
+            string wyn1 = wzrost > 180 ? "wysoki" : "niski";
+
+            string wyn2 = wzrost > 180 ? "wysoki" : wzrost < 160 ? "niski" : "średni";
+
+
+
+        }
+    }
+}
---
+++ P09Petle/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P09Petle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+
+            while (k< 10)
+            {
+                k++;
+                Console.WriteLine(k);
+            }
+
+            // zastanaowmy sie nad problemem z zdania p04
+
+            
+
+            string rownanie = "10 200 400";
+             
+            int pozycjaSpacji= -1;
+            int i = 0;
+            while (i < rownanie.Length) 
+            {
+                if (rownanie.Substring(i, 1) == " ")
+                    pozycjaSpacji = i;
+                
+                i++;
+            }
+
+
+            // to jest algorytm ktory dziala jak podobnie jak lastIndexOf
+
+
+            // chcemy zrobic dokładnie index of 
+            pozycjaSpacji = -1;
+            i = 0;
+            while (i < rownanie.Length)
+            {
+                if (rownanie.Substring(i, 1) == " ")
+                {
+                    pozycjaSpacji = i;
+                    i = rownanie.Length;
+                }
+
+                i++;
+            }
+            //
+
+            // chcemy zrobic dokładnie index of 
+            pozycjaSpacji = -1;
+            i = 0;
+            bool dziala = true;
+            while (dziala)
+            {
+                if (rownanie[i] == ' ')
+                {
+                    pozycjaSpacji = i;
+                    dziala = false;
+                }
+
+                i++;
+            }
+
+            string napis = "ala ma kota";
+            char znak = 'n';
+        }
+    }
+}
