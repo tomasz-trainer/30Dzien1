c22ddf713abd5c502d21bf047f7724db1bb39abe
csharp <csharp@406-00>
2026-06-18 10:54:37 +0200

    Add project files.

---
+++ P01Wstep/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01Wstep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string zmiennaDoPrzechowywaniaNapisow = "ala ma kota";
+            int zmiennaLiczbowa = 5;
+
+            //Console.WriteLine("ala ma kota");
+
+            Console.WriteLine(zmiennaDoPrzechowywaniaNapisow);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P03WczytywanieDanych/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03WczytywanieDanych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj jak sie nazywasz");
+            string imie = Console.ReadLine();//.ToUpper();
+
+            imie = imie.Substring(0, 1).ToUpper() + imie.Substring(1).ToLower();
+
+            //imie = imie.ToUpper();
+
+            Console.WriteLine(imie);
+
+            Console.WriteLine("Podaj wiek");
+            string wiek = Console.ReadLine();
+
+            //  wiek = wiek + 1;
+
+            int wiekInt = Convert.ToInt32(wiek) + 1;
+
+            
+
+            Console.WriteLine("Twoj wiek to za rok: " + wiekInt);
+        }
+    }
+}
